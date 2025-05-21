757ebb33ec8e53f21106a9c40e9bf8ceb90fab82
P.Henrique <paulocpm2014@gmaol.com>
2025-05-21 12:51:29 -0300

    feat: implementacao do consumo da API e gravação dos dados em arquivo JSON, e um tratamento de erro

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -37,6 +37,31 @@ String url = "https://ws.audioscrobbler.com/2.0/?method=album.getInfo" +
     "&album=" + albumFormatado +
     "&format=json";
 
+    try {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response resposta = client.newCall(request).execute();
+
+        if (resposta.isSuccessful()) {
+            String dadosJson = resposta.body().string();
+
+            Dados dados = gson.fromJson(dadosJson, Dados.class);
+
+            List<Musica> tracklist  = dados.album().tracks().track();
+            System.out.println(dados.album());
+
+            FileWriter escrita = new FileWriter("musicas.json");
+            escrita.write(gson.toJson(dados.album()));
+            escrita.close();
+
+        } else {
+            System.out.println("erro ao buscar os dados da api. : " + resposta.code());
+        }
+    } catch (Exception e) {
+        System.out.println("Ocorreu um erro: " + e.getMessage());
+    }
 
     }
 }
